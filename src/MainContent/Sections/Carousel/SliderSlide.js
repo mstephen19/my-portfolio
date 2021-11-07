23d287e2342a0b8f928966104fcfbbcd9dfd1183
Matt <matthiasvstephens@gmail.com>
2021-11-08 00:11:37 +0100

    Projects section finished

--- src/MainContent/Sections/Carousel/SliderSlide.js
+++ src/MainContent/Sections/Carousel/SliderSlide.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import Bounce from 'react-reveal/Bounce';
 
 export default function SliderSlide(props) {
   return (
     <div className='keen-slider__slide'>
-      <a href={props.ghlink} target='_blank' rel='noreferrer'>
-        <button className='ghBtn'>
-          <FontAwesomeIcon icon={faGithub} />
-          {props.btntxt}
-        </button>
-      </a>
+      {/* This bounce is not working as expected here, but it does give a nice fade haha */}
+      <Bounce left>
+        <a href={props.ghlink} target='_blank' rel='noreferrer'>
+          <button className='ghBtn'>
+            <FontAwesomeIcon icon={faGithub} />
+            {props.btntxt}
+          </button>
+        </a>
+      </Bounce>
       <a href={props.link} target='_blank' rel='noreferrer'>
         <img src={props.image} alt='' />
       </a>
--- src/MainContent/Sections/Carousel/TheCarousel.js
+++ src/MainContent/Sections/Carousel/TheCarousel.js
@@ -9,6 +9,9 @@ import SliderSlide from './SliderSlide';
 
 import haunted from '../assets/haunted-demo.gif';
 import notes from '../assets/notes-demo.gif';
+import charity from '../assets/charity-demo.gif';
+import omegle from '../assets/omegle-demo.gif';
+import api from '../assets/api-demo.gif';
 
 export default function TheCarousel() {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -33,9 +36,24 @@ export default function TheCarousel() {
       ghlink: 'https://github.com/alonzofroman/haunted-forum',
     },
     {
+      image: api,
+      link: 'https://github.com/mstephen19/ecommerce-back-end',
+      ghlink: 'https://github.com/mstephen19/ecommerce-back-end',
+    },
+    {
+      image: charity,
+      link: 'alonzofroman.github.io/charities-project/',
+      ghlink: 'https://github.com/alonzofroman/charities-project',
+    },
+    {
       image: notes,
       link: 'https://salty-plateau-23106.herokuapp.com/notes',
       ghlink: 'https://github.com/mstephen19/noteTaker',
+    },
+    {
+      image: omegle,
+      link: 'https://www.linkedin.com/posts/mstephen19_javascript-project-video-activity-6846827501633855488-_tJj',
+      ghlink: 'https://github.com/mstephen19/omegle-Chat-Bot',
     },
   ];
 
